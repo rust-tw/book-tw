47ddad73d2f7cb61d02455e967d637a711b8db14
Ngo Iok Ui <wusyong9104@gmail.com>
2020-10-08 23:21:47 +0800

    Update 04-01

--- listings/ch04-understanding-ownership/no-listing-01-can-mutate-string/src/main.rs
+++ listings/ch04-understanding-ownership/no-listing-01-can-mutate-string/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     // ANCHOR: here
     let mut s = String::from("hello");
 
-    s.push_str(", world!"); // push_str() appends a literal to a String
+    s.push_str(", world!"); // push_str() 將字面值加到字串後面
 
-    println!("{}", s); // This will print `hello, world!`
+    println!("{}", s); // 這會印出 `hello, world!`
                        // ANCHOR_END: here
 }
