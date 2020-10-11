b427ac256f01460f466b09424a3dd2c8c791e7f0
dklassic <dklassic@gmail.com>
2020-10-11 22:41:30 +0800

    Proof reading on ch05-03
    Improved readability

--- listings/ch05-using-structs-to-structure-related-data/listing-05-14/src/main.rs
+++ listings/ch05-using-structs-to-structure-related-data/listing-05-14/src/main.rs
@@ -12,6 +12,6 @@ fn main() {
         height: 45,
     };
 
-    println!("rect1 能容納rect2？{}", rect1.can_hold(&rect2));
-    println!("rect1 能容納rect3？{}", rect1.can_hold(&rect3));
+    println!("rect1 能容納 rect2 嗎？{}", rect1.can_hold(&rect2));
+    println!("rect1 能容納 rect3 嗎？{}", rect1.can_hold(&rect3));
 }
